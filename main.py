89b18c857d82eebc3fc1437cc73ecdbfdac33270
ai-lien <a.trancong@gmail.com>
2023-05-05 09:26:56 -0700

    create is_valid_credentials function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+def is_valid_credentials(username, password):
+    if username == "robert" and password == "password123":
+        return "You've unlocked the system."
+    else:
+        return "Scram!"
+
 if __name__=="__main__":
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    if username == "robert" and password == "password123":
-        print("You've unlocked the system.")
-    else:
-        print("Scram!")
+    print(is_valid_credentials(username, password))
