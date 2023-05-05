ed7b18093d8de8f8e198001c94c0d4709778ab4c
ai-lien <a.trancong@gmail.com>
2023-05-05 09:23:50 -0700

    add main.py with single user check

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__=="__main__":
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username == "robert" and password == "password123":
+        print("You've unlocked the system.")
+    else:
+        print("Scram!")
