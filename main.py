026e891badd6a46e41e9caf56783685f135ab666
ai-lien <a.trancong@gmail.com>
2023-05-05 14:32:02 -0700

    update is_valid_credentials to query username and password

--- main.py
+++ main.py
@@ -9,24 +9,22 @@ with open("db_config.yaml", "r") as file:
     db_path = config[0]["db_path"]
 
 def is_valid_credentials(username, password):
-    password_hash = hashlib.sha256(bytearray(password, "utf-8")).hexdigest()
-    
     db_connection = sqlite3.connect(db_path)
     cursor = db_connection.cursor()
-    result = cursor.execute(f"SELECT username, password_hash FROM users WHERE username = '{username}'")
-    existing_user = result.fetchall()
 
-    stored_credentials = None
-    if len(existing_user) > 0:
-        stored_credentials = {
-            "username": existing_user[0][0],
-            "password_hash": existing_user[0][1]
-            }
+    password_hash = hashlib.sha256(bytearray(password, "utf-8")).hexdigest()
+    result = cursor.execute(f"""
+        SELECT 
+        username,
+        password_hash
+        FROM 
+        users 
+        WHERE
+        username = '{username}'
+        AND
+        password_hash = '{password_hash}'""").fetchone()
     
-    if stored_credentials is None:
-        return False
-    
-    return stored_credentials["password_hash"] == password_hash
+    return result is not None
 
 
 if __name__=="__main__":
