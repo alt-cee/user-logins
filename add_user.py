457229cbb96be2259e0fbf75490d813ea9abbe13
ai-lien <a.trancong@gmail.com>
2023-05-05 13:36:40 -0700

    add check to see if user exists in db

--- add_user.py
+++ add_user.py
@@ -3,11 +3,21 @@ import sqlite3
 
 
 if __name__ == "__main__":
-    username = input("Enter a username: ")
+    db_connection = sqlite3.connect("ppab6.db")
+    cursor = db_connection.cursor()
+
+    valid_username = False
+    while not valid_username:
+        username = input("Enter a username: ")
+        result = cursor.execute(f"SELECT username FROM users WHERE username = '{username}'")
+        existing_users = result.fetchall()
+        if len(existing_users) == 0:
+            valid_username = True
+        else:
+            print("Not a valid username.")
+    
     password = input("Enter a password: ")
     password_hash = hashlib.sha256(bytearray(password, "utf-8")).hexdigest()
 
-    db_connection = sqlite3.connect("ppab6.db")
-    cursor = db_connection.cursor()
     cursor.execute(f"INSERT INTO users VALUES ('{username}', '{password_hash}');")
     db_connection.commit()
