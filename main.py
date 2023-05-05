7130b88a10ea3bda120ac142adec2b8575c15d6f
ai-lien <a.trancong@gmail.com>
2023-05-05 10:32:41 -0700

    add hashing to is_valid_credentials

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import hashlib
+
 def is_valid_credentials(username, password):
     credentials = {
-        "robert": "password123",
-        "anoosh": "snuffles456"
+        "robert": "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f",
+        "anoosh": "3c32608fd9ffd87ae17dbb3a65a509cc8ba5aa395147c99ed20bdba9c434d8c2"
     }
-    if (username in credentials) and credentials[username] == password:
-        return "You've unlocked the system."
+    if (username in credentials
+        and credentials[username] == hashlib.sha256(bytearray(password, "utf-8")).hexdigest()):
+        return True
     else:
-        return "Scram!"
+        return False
 
 
 if __name__=="__main__":
