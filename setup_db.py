2eb55f5e85191cabd0872aa3654638c05fea3fc7
ai-lien <a.trancong@gmail.com>
2023-05-05 11:33:00 -0700

    add logic to setup_db.py to check if db exists

--- setup_db.py
+++ setup_db.py
@@ -1,10 +1,22 @@
+import os
 import sqlite3
 
+db_filename = "ppab6.db"
 
 if __name__ == "__main__":
-    db_connection = sqlite3.connect("ppab6.db")
-    cursor = db_connection.cursor()
+    if os.path.isfile(db_filename):
+        db_connection = sqlite3.connect(db_filename)
+        cursor = db_connection.cursor()
+        result = cursor.execute("SELECT COUNT(username) FROM users;")
+        n_rows = result.fetchone()[0]
+        print(f"Database already exists with {n_rows} rows.")
 
-    cursor.execute("CREATE TABLE users(username VARCHAR, password_hash VARCHAR);")
-
-
+        delete_db = None
+        while delete_db not in ['y', 'n']:
+            delete_db = input("Do you want to delete and recreate the database? [y/n]: ")
+        
+        if delete_db == 'y':
+            os.remove(db_filename)
+            db_connection = sqlite3.connect(db_filename)
+            cursor = db_connection.cursor()
+            cursor.execute("CREATE TABLE users(username VARCHAR, password_hash VARCHAR);")
