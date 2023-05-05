8ad26de96bffb3c996de3c1b12a7001747def394
ai-lien <a.trancong@gmail.com>
2023-05-05 14:22:34 -0700

    refactor is_valid_credentials to use stored_credentials object

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ with open("db_config.yaml", "r") as file:
     db_path = config[0]["db_path"]
 
 def is_valid_credentials(username, password):
+    password_hash = hashlib.sha256(bytearray(password, "utf-8")).hexdigest()
+    
     db_connection = sqlite3.connect(db_path)
     cursor = db_connection.cursor()
     result = cursor.execute(f"SELECT username, password_hash FROM users WHERE username = '{username}'")
     existing_user = result.fetchall()
-    password_hash = hashlib.sha256(bytearray(password, "utf-8")).hexdigest()
+
+    stored_credentials = None
     if len(existing_user) > 0:
-        if password_hash == existing_user[0][1]:
-            return True
-    return False
+        stored_credentials = {
+            "username": existing_user[0][0],
+            "password_hash": existing_user[0][1]
+            }
+    
+    if stored_credentials is None:
+        return False
+    
+    return stored_credentials["password_hash"] == password_hash
 
 
 if __name__=="__main__":
