bc06cad3f31d5ec4219d82f8dc50625e3c4ac684
ai-lien <a.trancong@gmail.com>
2023-05-05 10:24:17 -0700

    update hashing.py to hash passwords from credentials dict

--- hashing.py
+++ hashing.py
@@ -1,8 +1,11 @@
 import hashlib
 
-if __name__ == "__main__":
-    b = bytearray('steveslist', 'utf-8')
-    print(hashlib.sha256(b).hexdigest())
-    print(hashlib.sha256(b).hexdigest())
-    print(hashlib.md5(b).hexdigest())
+credentials = {
+    "robert": "password123",
+    "anoosh": "snuffles456"
+}
 
+if __name__ == "__main__":
+    for username, password in credentials.items():
+        b = bytearray(password, 'utf-8')
+        print(username, hashlib.sha256(b).hexdigest())
