45a25413e72218f51a9e4a35ce127f7a39760f1e
ai-lien <a.trancong@gmail.com>
2023-05-05 09:44:00 -0700

    add ability to check multiple users

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def is_valid_credentials(username, password):
-    if username == "robert" and password == "password123":
+    credentials = {
+        "robert": "password123",
+        "anoosh": "snuffles456"
+    }
+    if (username in credentials) and credentials[username] == password:
         return "You've unlocked the system."
     else:
         return "Scram!"
+
 
 if __name__=="__main__":
     username = input("Enter your username: ")
